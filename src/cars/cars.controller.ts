12d3bd9e516fd0f23f7ec3c2aeee53d9c883adfc
Isaias Medina <isamed92@gmail.com>
2024-06-15 00:30:42 -0300

    feat: add new endpoints for car CRUD

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
+  @Get(':id')
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.findOneById(id);
+  }
+
   @Get()
   getAllCars() {
-    return ['car1', 'car2', 'car3'];
+    return this.carsService.findAll();
+  }
+
+  @Post()
+  createCar(@Body() body: any, @Param('id', ParseIntPipe) id: number) {
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar(@Body() body: any) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'DELETE',
+      message: `Car ${id} has been deleted`,
+    };
   }
 }
